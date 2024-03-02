4b9dec2d8ee630a6c4d166e449a0c2d94066defd
Sharjeel-Afridi <afridisharjeel8@gmail.com>
2024-03-02 17:15:26 +0530

    converted nav button to components

--- script.js
+++ script.js
@@ -1,10 +1,33 @@
 const navBtns = document.querySelectorAll('.nav-btn');
+const contentDiv = document.querySelector('#content');
+
+const homeComponent = `<h1 class="font-bold text-2xl mt-[80px] ml-[50px]">HOME</h1>`;
+const mydriveComponent = `<h1 class="font-bold text-2xl mt-[80px] ml-[50px]">My Drive</h1>`;
+const recentComponent = `<h1 class="font-bold text-2xl mt-[80px] ml-[50px]">Recent</h1>`;
+
+const components = {
+    "HOME": `<h1 class="font-bold text-2xl mt-[80px] ml-[50px]">HOME</h1>`,
+    "My Drive": `<h1 class="font-bold text-2xl mt-[80px] ml-[50px]">My Drive</h1>`,
+    "Recent": `<h1 class="font-bold text-2xl mt-[80px] ml-[50px]">Recent</h1>`
+};
+
+function renderComponent(component){
+    contentDiv.innerHTML = component;
+}
 
 navBtns.forEach(navBtn => {
-    navBtn.addEventListener('click', () => {
+    navBtn.addEventListener('click', (event) => {
+        let buttonText = event.target.textContent.trim();
+        let buttonComponent = components[buttonText];
+        
+        if (buttonComponent) {
+            renderComponent(buttonComponent);
+        }
         navBtns.forEach(btn => {
             btn.classList.remove("text-black", "border-white", "rounded-full", "bg-blue-100");
+
         });
         navBtn.classList.add("text-black", "border-white", "rounded-full", "bg-blue-100");
     });
 });
+
