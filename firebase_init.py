2fba85372b2e937ac066ae156f9f174075abeead
Sharjeel-Afridi <afridisharjeel8@gmail.com>
2024-03-03 20:16:16 +0530

    minor fixes

--- firebase_init.py
+++ firebase_init.py
@@ -4,3 +4,6 @@ from firebase_admin import credentials, storage
 cred = credentials.Certificate('serviceAccountKey.json')
 firebase_admin.initialize_app(cred)
 
+storage_client = storage.bucket('cryptodrive-team96.appspot.com')
+
+blobs = storage_client.list_blobs()
